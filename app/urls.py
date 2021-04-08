3fcf0d8be582f76f6baba78823c60f34611c8d5d
higakijin <tellstyle.jp@gmail.com>
2021-04-08 21:58:48 +0900

    nine

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.welcome),
     path('index/', views.index, name='index'),
-    path('index/new', views.NewPost.as_view(), name="new_post"),
+    path('new/', views.NewPost.as_view(), name="new"),
 ]
