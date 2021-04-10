b6ceafbafefb2bc78cb3f8a7f20566c111064fdb
higakijin <tellstyle.jp@gmail.com>
2021-04-10 13:58:38 +0900

    24

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.welcome),
     path('index/', views.index, name='index'),
-    path('new/', views.NewPost.as_view(), name="new"),
+    path('new/', views.FormPost.as_view(), name="form"),
     path('post/<int:pk>', views.PostDetail.as_view(), name='detail'),
+    path('post/<int:pk>/edit', views.PostEdit.as_view(), name='edit'),
     path('post/<int:pk>/delete', views.PostDelete.as_view(), name="delete"),
 ]
--- app/views.py
+++ app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import View,DetailView
 from .models import Post
 from .forms import PostForm
-# from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def welcome(request):
@@ -15,10 +16,10 @@ def index(request):
   }
   return render(request, 'app/index.html', params)
 
-class NewPost(View):
+class FormPost(View):
   def get(self, request, *args, **kwargs):
     form = PostForm(request.POST or None)
-    return render(request, 'app/new.html', {'form': form})
+    return render(request, 'app/form.html', {'form': form})
 
   def post(self, request, *args, **kwargs):
     form = PostForm(request.POST or None)
@@ -32,7 +33,7 @@ class NewPost(View):
       post_data.save()
       return redirect('index')
 
-    return render(request, 'app/new.html', {'form': form})
+    return render(request, 'app/form.html', {'form': form})
 
 class PostDetail(View):
   def get(self, request, *args, **kwargs):
@@ -41,7 +42,36 @@ class PostDetail(View):
       'post_data': post_data,
     })
   
-class PostDelete(View):
+class PostEdit(LoginRequiredMixin, View):
+  def get(self, request, *args, **kwargs):
+    post_data = Post.objects.get(id=self.kwargs['pk'])
+    form = PostForm(
+      request.POST or None,
+      initial = {
+        'year': post_data.year,
+        'text': post_data.text,
+        'supplement': post_data.supplement,
+      }
+    )
+    return render(request, 'app/form.html', {
+      'form': form
+    })
+  def post(self, request, *args, **kwargs):
+    form = PostForm(request.POST or None)
+
+    if form.is_valid():
+      post_data = Post.objects.get(id=self.kwargs['pk'])
+      post_data.author = request.user
+      post_data.year = form.cleaned_data['year']
+      post_data.text = form.cleaned_data['text']
+      post_data.supplement = form.cleaned_data['supplement']
+      post_data.save()
+      return redirect('detail', self.kwargs['pk'])
+
+    return render(request, 'app/form.html', {'form': form})
+
+
+class PostDelete(LoginRequiredMixin, View):
   def get(self, request, *args, **kwargs):
     post_data = Post.objects.get(id=self.kwargs['pk'])
     return render(request, 'app/delete.html', {
